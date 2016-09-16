684ba85413490d5f5827166e2b34b878ba5d29ed
Maryam <aminian.mary@gmail.com>
2016-09-15 20:15:54 -0400

    added early stopping to reranker train

--- src/SupervisedSRL/Reranker/Train.java
+++ src/SupervisedSRL/Reranker/Train.java
@@ -1,7 +1,11 @@
 package SupervisedSRL.Reranker;
 
+import SupervisedSRL.Evaluation;
+import SupervisedSRL.Strcutures.IndexMap;
+import SupervisedSRL.Strcutures.ModelInfo;
 import SupervisedSRL.Strcutures.Properties;
 import SupervisedSRL.Strcutures.RerankerFeatureMap;
+import ml.AveragedPerceptron;
 import ml.RerankerAveragedPerceptron;
 import util.IO;
 
@@ -9,6 +13,8 @@ import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.ObjectInput;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.zip.GZIPInputStream;
 
 /**
@@ -21,6 +27,8 @@ public class Train {
         String rerankerModelPath = properties.getRerankerModelPath();
 
         RerankerAveragedPerceptron ap = new RerankerAveragedPerceptron(numOfFeatures(properties));
+        double bestFScore = 0;
+        int noImprovement = 0;
 
         for (int iter = 0; iter < numOfTrainingIterations; iter++) {
             System.out.println("Iteration " + iter + "\n>>>>>>>>>>>\n");
@@ -41,8 +49,44 @@ public class Train {
                 }
                 System.out.println("Part " + devPart + " done!");
             }
+
+            System.out.print("Making prediction on Dev data...");
+            HashMap<Object, Integer>[] rerankerFeatureMap = ((RerankerFeatureMap) IO.load(properties.getRerankerFeatureMapPath())).getFeatureMap();
+            AveragedPerceptron aiClassifier = AveragedPerceptron.loadModel(properties.getAiModelPath());
+            AveragedPerceptron acClassifier = AveragedPerceptron.loadModel(properties.getAcModelPath());
+            IndexMap indexMap = IO.load(properties.getIndexMapFilePath());
+            String pdModelDir = properties.getPdModelDir();
+            ArrayList<String> devSentences = IO.readCoNLLFile(properties.getDevFile());
+            int numOfPDFeatures = properties.getNumOfPDFeatures();
+            int numOfAIFeatures = properties.getNumOfAIFeatures();
+            int numOfACFeatures = properties.getNumOfACFeatures();
+            int numOfGlobalFeatures= properties.getNumOfGlobalFeatures();
+            int aiMaxBeamSize = properties.getNumOfAIBeamSize();
+            int acMaxBeamSize = properties.getNumOfACBeamSize();
+            String outputFile = properties.getOutputFilePath() + "_"+iter;
+            HashMap<String, Integer> globalReverseLabelMap = IO.load(properties.getGlobalReverseLabelMapPath());
+
+            SupervisedSRL.Reranker.Decoder decoder = new SupervisedSRL.Reranker.Decoder(aiClassifier, acClassifier, ap, indexMap, rerankerFeatureMap, pdModelDir);
+            decoder.decode(devSentences, numOfPDFeatures, numOfAIFeatures, numOfACFeatures, numOfGlobalFeatures, aiMaxBeamSize, acMaxBeamSize, pdModelDir, outputFile);
+
+            HashMap<String, Integer> reverseLabelMap = new HashMap<String, Integer>(globalReverseLabelMap);
+            reverseLabelMap.put("0", reverseLabelMap.size());
+            double f1= Evaluation.evaluate(outputFile, devSentences, indexMap, reverseLabelMap);
+
+            if (f1 > bestFScore) {
+                noImprovement = 0;
+                bestFScore = f1;
+                System.out.print("\nSaving the new model...");
+                ap.saveModel(rerankerModelPath);
+                System.out.println("Done!");
+            } else {
+                noImprovement++;
+                if (noImprovement > 2) {
+                    System.out.print("\nEarly stopping...");
+                    break;
+                }
+            }
         }
-        ap.saveModel(rerankerModelPath);
     }
 
     // todo this is not efficient.
